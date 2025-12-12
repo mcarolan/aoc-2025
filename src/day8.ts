f2d507fe046190f59390587a1e09d69f7f27569e
Martin Carolan <martin@carolan.email>
2025-12-12 09:07:28 +0000

    wip

--- src/day8.ts
+++ src/day8.ts
@@ -125,3 +125,103 @@ export function part1(input: string, n: number): number {
     sizes[sizes.length - 3]!
   );
 }
+
+export function part2(input: string): number {
+  const coordinates = splitLines(input).map((l) =>
+    coordFromArray(l.split(",").map(Number)),
+  );
+
+  type Distance = { from: Coord; to: Coord; value: number };
+  let calculatedDistances: ImmSet<List<Coord>> = ImmSet();
+  let distances: List<Distance> = List();
+
+  for (const coord1 of coordinates) {
+    for (const coord2 of coordinates) {
+      if (is(coord1, coord2)) {
+        continue;
+      }
+
+      const fromTo = List([coord1, coord2]).sort();
+      const from = fromTo.get(0)!;
+      const to = fromTo.get(1)!;
+
+      if (!calculatedDistances.has(fromTo)) {
+        calculatedDistances = calculatedDistances.add(fromTo);
+        distances = distances.push({
+          from,
+          to,
+          value: straightLineDistance(from, to),
+        });
+      }
+    }
+  }
+
+  distances = distances.sortBy((d) => d.value);
+
+  const neighbourhoodsByNode = new Map<string, number>();
+  const nodesByNeighbourhoods = new Map<number, Set<string>>();
+  const activeNeighbourhood = new Set<number>();
+
+  let counter = 0;
+  let entry: Distance | undefined;
+  let i = 0;
+
+  while (i < distances.size) {
+    entry = distances.get(i++)!;
+
+    const from = coordToString(entry.from);
+    const to = coordToString(entry.to);
+    const fromNeighbourhood = neighbourhoodsByNode.get(from);
+    const toNeighbourhood = neighbourhoodsByNode.get(to);
+
+    if (fromNeighbourhood === undefined && toNeighbourhood === undefined) {
+      const newNeighbourhood = counter++;
+      neighbourhoodsByNode.set(from, newNeighbourhood);
+      neighbourhoodsByNode.set(to, newNeighbourhood);
+      nodesByNeighbourhoods.set(newNeighbourhood, new Set([from, to]));
+      activeNeighbourhood.add(newNeighbourhood);
+    } else if (
+      fromNeighbourhood === undefined &&
+      toNeighbourhood !== undefined
+    ) {
+      neighbourhoodsByNode.set(from, toNeighbourhood);
+      const toNeighbourSet = nodesByNeighbourhoods.get(toNeighbourhood);
+      toNeighbourSet?.add(from);
+    } else if (
+      fromNeighbourhood !== undefined &&
+      toNeighbourhood === undefined
+    ) {
+      neighbourhoodsByNode.set(to, fromNeighbourhood);
+      const fromNeighbourSet = nodesByNeighbourhoods.get(fromNeighbourhood);
+      fromNeighbourSet?.add(to);
+    } else if (
+      fromNeighbourhood !== undefined &&
+      toNeighbourhood !== undefined &&
+      fromNeighbourhood !== toNeighbourhood
+    ) {
+      const toKeep = Math.min(fromNeighbourhood, toNeighbourhood);
+      const toDiscard = Math.max(fromNeighbourhood, toNeighbourhood);
+
+      const toKeepSet = nodesByNeighbourhoods.get(toKeep);
+      for (const discard of nodesByNeighbourhoods.get(toDiscard)!) {
+        neighbourhoodsByNode.set(discard, toKeep);
+        toKeepSet?.add(discard);
+      }
+      nodesByNeighbourhoods.delete(toDiscard);
+
+      neighbourhoodsByNode.set(from, toKeep);
+      neighbourhoodsByNode.set(to, toKeep);
+      activeNeighbourhood.delete(toDiscard);
+    }
+
+    if (activeNeighbourhood.size == 1 && i > 1) {
+      break;
+    }
+  }
+
+  if (entry) {
+    return entry.from.x * entry.to.x;
+  } else {
+    return 0;
+  }
+}
--- test/day8.test.ts
+++ test/day8.test.ts
@@ -1,4 +1,5 @@
-import { part1 } from "../src/day8";
+import { skip } from "node:test";
+import { part1, part2 } from "../src/day8";
 import { readInput } from "./utils";
 
 describe("day 8", () => {
@@ -28,9 +29,36 @@ describe("day 8", () => {
       expect(part1(input, 10)).toBe(40);
     });
 
-    test("input", () => {
+    skip("input", () => {
       const input = readInput("day8");
       expect(part1(input, 1000)).toBe(122636);
+    });
+  });
+
+  describe("part 2", () => {
+    test("example", () => {
+      const input = `162,817,812
+          57,618,57
+          906,360,560
+          592,479,940
+          352,342,300
+          466,668,158
+          542,29,236
+          431,825,988
+          739,650,466
+          52,470,668
+          216,146,977
+          819,987,18
+          117,168,530
+          805,96,715
+          346,949,466
+          970,615,88
+          941,993,340
+          862,61,35
+          984,92,344
+          425,690,689`;
+
+      expect(part2(input)).toBe(42);
     });
   });
 });
